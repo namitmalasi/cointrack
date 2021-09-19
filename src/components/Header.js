20dad003b322a179579ca647cac2432fea8ad3cc
Namit Malasi <namitmalasi@gmail.com>
2021-09-19 13:56:57 +0530

    header component changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <h1 className="text-center text-warning mt-3 mb-4">CoinTrack</h1>
+      <Link to="/">
+        <h1 className="text-center text-warning mt-3 mb-4">CoinTrack</h1>
+      </Link>
     </div>
   );
 };
